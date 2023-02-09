5b15755829a5392f8bb64838ae494aadeca01fa7
Livan Jimenez <jimenezlivan14@gmail.com>
2023-02-09 11:14:54 -0500

    pushing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [customerData, setCustomerData] = useState([]);
+
+  const fetchAPI = async () => {
+    const res = await fetch('./MOCK_DATA.json');
+    const data = await res.json();
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  useEffect(() => {
+    const customerTotals = data.map((customer) => {
+      const months = {};
+      customer.purchases.forEach((purchase) => {
+        const month = purchase.date.slice(0, 11);
+        if (!months[month]) {
+          months[month] = 0;
+        }
+        if (purchase.amount >= 100) {
+          months[month] += (purchase.amount - 100) * 2 + 50;
+        } else if (purchase.amount >= 50 && purchase.amount <= 100) {
+          months[month] += purchase.amount - 50;
+        }
+      });
+      return {
+        name: customer.name,
+        months: months,
+        total: Object.values(months).reduce((sum, points) => sum + points, 0),
+      };
+    });
+
+    setCustomerData(customerTotals);
+  }, [data]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {customerData.map((data) => (
+        <ul>
+          {Object.entries(data.months).map(([month, points]) => (
+            <li key={data.customer_id}>
+              {data.name} = {month}: {points}
+            </li>
+          ))}
+          <li key={data.customer_id}>Total earned: {data.total}</li>
+        </ul>
+      ))}
     </div>
   );
 }
